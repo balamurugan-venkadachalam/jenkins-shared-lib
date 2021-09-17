bf2257db9e5c99aac20342e2e4062adfba38e2f0
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2021-09-18 10:06:33 +1200

    first commit

--- src/com/bala/Hello.groovy
+++ src/com/bala/Hello.groovy
@@ -13,6 +13,20 @@ class Hello {
     def sayHello(){
         script.println"###########################################"
         script.println"Hello Bala !!!!!!!!!"
+        script.println"Job starting"
         script.println"###########################################"
+        script.stage('build'){
+            script.println"build stage"
+        }
+        script.stage('deploy'){
+            script.println"deploy stage"
+        }
+        script.stage('release'){
+            script.println"release stage"
+        }
+        script.println"###########################################"
+        script.println"Job completed"
+        script.println"###########################################"
+
     }
 }
