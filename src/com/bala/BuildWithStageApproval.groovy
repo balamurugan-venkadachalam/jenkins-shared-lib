2bc1fe2bc166d104d2f7bcbf40fb78cce3b3d3d5
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2021-09-18 11:52:31 +1200

    build with stage approval

--- src/com/bala/BuildWithStageApproval.groovy
+++ src/com/bala/BuildWithStageApproval.groovy
@@ -24,11 +24,7 @@ class BuildWithStageApproval {
             script.println"testing..."
         }
         script.stage('Approval'){
-            script.println"deploying..."
-            steps: def steps(){
-                        def deploymentDelay = input id: 'Deploy', message: 'Deploy to production?', submitter: 'bala,admin', parameters: [choice(choices: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'], description: 'Hours to delay deployment?', name: 'deploymentDelay')]
-                        sleep time: deploymentDelay.toInteger(), unit: 'HOURS'
-                    }
+            steps: steps
         }
         script.stage('deploy'){
             script.println"deploying..."
@@ -40,5 +36,11 @@ class BuildWithStageApproval {
         script.println"Job completed"
         script.println"###########################################"
 
+    }
+
+    static private steps(){
+
+        def deploymentDelay = input id: 'Deploy', message: 'Deploy to production?', submitter: 'bala,admin', parameters: [choice(choices: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'], description: 'Hours to delay deployment?', name: 'deploymentDelay')]
+        sleep time: deploymentDelay.toInteger(), unit: 'HOURS'
     }
 }
