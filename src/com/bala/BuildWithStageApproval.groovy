dc926746247197eb4a1579b9c4efe4b2b912028a
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2021-09-18 10:45:30 +1200

    build with stage approval

--- src/com/bala/BuildWithStageApproval.groovy
+++ src/com/bala/BuildWithStageApproval.groovy
@@ -17,13 +17,20 @@ class BuildWithStageApproval {
         script.println"Hello Bala !!!!!!!!!"
         script.println"Job starting"
         script.println"###########################################"
-        script.stage('Build'){
+        script.stage('build'){
             script.println"compiling..."
         }
-        script.stage('Test'){
+        script.stage('test'){
             script.println"testing..."
         }
-        script.stage('Deploy'){
+        script.stage('Approval'){
+            script.println"deploying..."
+            steps(){
+                def deploymentDelay = input id: 'Deploy', message: 'Deploy to production?', submitter: 'bala,admin', parameters: [choice(choices: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'], description: 'Hours to delay deployment?', name: 'deploymentDelay')]
+                sleep time: deploymentDelay.toInteger(), unit: 'HOURS'
+            }
+        }
+        script.stage('deploy'){
             script.println"deploying..."
         }
         script.stage('release'){
